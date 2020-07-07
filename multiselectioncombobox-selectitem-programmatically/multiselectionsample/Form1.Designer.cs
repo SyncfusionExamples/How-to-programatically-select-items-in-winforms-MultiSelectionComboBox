5bc070e731b59872a31872bbfc3757c504f7cdd8
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-07-07 16:41:05 +0530

    Added sample for select item programmatically in MultiSelectionComboBox

---
+++ multiselectioncombobox-selectitem-programmatically/multiselectionsample/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace multiselectionsample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.multiSelectionComboBox1 = new Syncfusion.Windows.Forms.Tools.MultiSelectionComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.multiSelectionComboBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // multiSelectionComboBox1
+            // 
+            this.multiSelectionComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.multiSelectionComboBox1.BeforeTouchSize = new System.Drawing.Size(162, 30);
+            this.multiSelectionComboBox1.ButtonStyle = Syncfusion.Windows.Forms.ButtonAppearance.Office2016Colorful;
+            this.multiSelectionComboBox1.DataSource = ((object)(resources.GetObject("multiSelectionComboBox1.DataSource")));
+            this.multiSelectionComboBox1.DisplayMode = Syncfusion.Windows.Forms.Tools.DisplayMode.NormalMode;
+            this.multiSelectionComboBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiSelectionComboBox1.Location = new System.Drawing.Point(287, 180);
+            this.multiSelectionComboBox1.Name = "multiSelectionComboBox1";
+            this.multiSelectionComboBox1.ShowCheckBox = true;
+            this.multiSelectionComboBox1.Size = new System.Drawing.Size(162, 30);
+            this.multiSelectionComboBox1.TabIndex = 0;
+            this.multiSelectionComboBox1.ThemeName = "Office2016Colorful";
+            this.multiSelectionComboBox1.UseVisualStyle = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.multiSelectionComboBox1);
+            this.Name = "Form1";
+            this.Text = "MultiSelectionComboBox Sample";
+            ((System.ComponentModel.ISupportInitialize)(this.multiSelectionComboBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.MultiSelectionComboBox multiSelectionComboBox1;
+    }
+}
+
---
+++ multiselectioncombobox-selectitem-programmatically/multiselectionsample/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiselectionsample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            multiSelectionComboBox1.ShowCheckBox = true;
+            multiSelectionComboBox1.DataSource = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            multiSelectionComboBox1.SelectedItem = multiSelectionComboBox1.Items[4];
+
+        }
+
+        private void Form1_Load1(object sender, EventArgs e)
+        {
+            List<string> tempList = new List<string>();
+            for (int i = 0; i < this.multiSelectionComboBox1.Items.Count; i++)
+            {
+                var value = this.multiSelectionComboBox1.Items[i];
+                if ((value.Equals("Monday") && !tempList.Contains("Monday")) || (value.Equals("Tuesday") && !tempList.Contains("Tuesday")))
+                {
+                    tempList.Add(value.ToString());
+                    this.multiSelectionComboBox1.SelectedItems.Add(value.ToString());
+                }
+            }
+        }
+    }
+}
